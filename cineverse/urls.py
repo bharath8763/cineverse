a064bd8b4fd4271f75e1df24d203216540e94de6
Bharath <sbvsatish@gmail.com>
2025-12-23 20:48:23 +0530

    fix root urls and routing

--- cineverse/urls.py
+++ cineverse/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
-
+from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
